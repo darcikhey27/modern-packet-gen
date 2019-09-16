6689c7f8fa87f5ea888ff3090763139fe3001583
darci <darcikhey27@gmail.com>
2019-09-15 22:32:35 -0700

    Moved files around for flask templating

---
+++ src/api/templates/main.js
@@ -0,0 +1,13 @@
+l2_protocols = {
+    arp:{},
+    ether: {
+        "dest" : "ff:ff:ff:ff:ff:ff",
+        "src" : "ff:ff:ff:ff:ff:ff",
+        "dot1q_tag" : 0,
+        "etype" : 0x9000,
+        "payload" : ""
+    }
+    // Add more
+}
+
+l3_protocols
