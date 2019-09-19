9f238b637a64958afd3d40c56010eb09210d7763
darci <darcikhey27@gmail.com>
2019-09-18 23:13:48 -0700

    Added some dummy html

--- src/api/static/js/main.js
+++ src/api/static/js/main.js
@@ -1,10 +1,14 @@
 
-$(document).ready(function () {
-    
+$(document).ready((init))
+
+function init() {
     showInterfacesList();
-})
+}
+    
+    
 
 function showInterfacesList() {
     // call flask api for a list of interfaces on the system
-    
+    interface = "<option>eth0</option>"
+    $(".interface_list").append(interface)
 }
